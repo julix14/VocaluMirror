e538cd4c08b01f0336429f33fd326c9fb097a4ad
Juliusbecker <julix.becker@gmail.com>
2023-07-03 18:53:16 +0200

    Fix Workflow and server.js

--- src/server/main.js
+++ src/server/main.js
@@ -1,5 +1,5 @@
-import express from "express";
-import { listen } from "vite-express";
+const express = require("express");
+const ViteExpress = require("vite-express");
 
 const app = express();
 
@@ -7,4 +7,6 @@ app.get("/hello", (req, res) => {
     res.send("Hello Vite + Vue!");
 });
 
-listen(app, 3000, () => console.log("Server is listening on port 3000..."));
+ViteExpress.listen(app, 3000, () =>
+    console.log("Server is listening on port 3000...")
+);
