c524a17fbdea30c889de743293dfbb4967e8d011
Juliusbecker <julix.becker@gmail.com>
2023-07-04 15:19:16 +0200

    Add Orange to TailwindConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"];
-export const theme = { extend: {} };
+export const theme = {
+    extend: {
+        colors: {
+            orange: "#FF862E",
+        },
+    },
+};
 export const plugins = [];
