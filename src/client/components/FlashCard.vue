93563b47046c1c87ae560b6ce870b1666b1321ef
Juliusbecker <julix.becker@gmail.com>
2023-07-05 13:32:52 +0200

    Add Mocked Flash Card Values to component

---
+++ src/client/components/FlashCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-white rounded-3xl w-1/2 h-1/2 mt-5 p-2 flex flex-col drop-shadow-2xl">
+        <p class="text-xl">{{ flashCard.topic }}</p>
+        <p class="text-2xl text-center">{{ flashCard.question }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    flashCard: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
