5fa85c5c2f52536e779edb312b331e3782f4dfa6
Juliusbecker <julix.becker@gmail.com>
2023-07-05 18:22:30 +0200

    Change Flash Card Layout

--- src/client/components/FlashCard.vue
+++ src/client/components/FlashCard.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="bg-white rounded-3xl w-1/2 h-1/2 mt-5 p-2 flex flex-col drop-shadow-2xl">
-        <p class="text-xl">{{ flashCard.topic }}</p>
-        <p class="text-2xl text-center">{{ flashCard.question }}</p>
+    <div class="bg-white rounded-3xl w-1/2 h-1/2 mt-5 p-2 drop-shadow-2xl">
+        <div class="h-full flex flex-col justify-between">
+            <div class="grid grid-cols-3 justify-items-center p-2">
+                <p class="text-xl">{{ flashCard.topic }}</p>
+                <p class="text-xl col-start-3">Stage {{ flashCard.stage }}</p>
+            </div>
+            <p class="text-3xl p-2 text-center">{{ flashCard.question }}</p>
+            <div>&nbsp;</div>
+        </div>
     </div>
 </template>
 
