13cdc72497ca52ff5da6bc8ebfe44599dd3fa313
Juliusbecker <julix.becker@gmail.com>
2023-07-06 19:35:58 +0200

    Small fixes

--- src/client/pages/FlashcardPage.vue
+++ src/client/pages/FlashcardPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="bg-lightBlue bg-opacity-75 w-screen h-screen flex flex-col justify-start items-center container">
+        class="bg-lightBlue bg-opacity-75 w-screen h-screen flex flex-col justify-start items-center">
         <div>
             <p class="text-white text-9xl text-center mt-10 drop-shadow-2xl">Flash Cards</p>
             <p class="text-white text-4xl text-center font-bold mt-4">
@@ -118,9 +118,3 @@ const flashCards = [
     },
 ];
 </script>
-
-<style scoped>
-.container {
-    perspective: 1000px;
-}
-</style>
