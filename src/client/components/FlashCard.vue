b40df23be1cf140a499c1a40da1fcf942e89a30f
Juliusbecker <julix.becker@gmail.com>
2023-07-06 19:22:24 +0200

    Add Animation

--- src/client/components/FlashCard.vue
+++ src/client/components/FlashCard.vue
@@ -2,10 +2,10 @@
     <Transition
         enter-from-class="rotate-y-90"
         enter-to-class="rotate-y-0"
-        enter-active-class="transition-transform delay-150 duration-300 "
+        enter-active-class="transition-transform duration-500 "
         leave-from-class="rotate-y-0"
         leave-to-class="rotate-y-90 hidden"
-        leave-active-class="transition-transform duration-300 ">
+        leave-active-class="transition-transform duration-500 ">
         <div v-if="!showBackside" class="bg-white rounded-3xl w-1/2 h-1/2 mt-5 p-6 drop-shadow-2xl">
             <div class="h-full flex flex-col justify-between">
                 <div class="grid grid-cols-3 p-2">
@@ -30,10 +30,10 @@
     <Transition
         enter-from-class="rotate-y-90"
         enter-to-class="rotate-y-0"
-        enter-active-class="transition-transform delay-150 duration-300 "
+        enter-active-class="transition-transform duration-500 "
         leave-from-class="rotate-y-0"
         leave-to-class="rotate-y-90"
-        leave-active-class="transition-transform duration-300 hidden">
+        leave-active-class="transition-transform duration-500 hidden">
         <div v-if="showBackside" class="bg-white rounded-3xl w-1/2 h-1/2 mt-5 p-6 drop-shadow-2xl">
             <div class="h-full flex flex-col justify-between">
                 <div class="grid grid-cols-3 p-2">
@@ -70,4 +70,50 @@ defineProps({
 const showBackside = ref(false);
 </script>
 
-<style scoped></style>
+<style scoped>
+.flip-card {
+    background-color: transparent;
+    width: 300px;
+    height: 200px;
+    border: 1px solid #f1f1f1;
+    perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front,
+.flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden; /* Safari */
+    backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+    background-color: #bbb;
+    color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+    background-color: dodgerblue;
+    color: white;
+    transform: rotateY(180deg);
+}
+</style>
