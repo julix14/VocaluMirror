1fce39dcd90ca767bc89187c334336edaf182853
Juliusbecker <julix.becker@gmail.com>
2023-07-19 20:01:29 +0200

    Add flashCard Table

---
+++ migrations/1689789546378_createFlashCardTable.js
@@ -0,0 +1,20 @@
+exports.up = (pgm) => {
+    pgm.createTable("flash_card", {
+        id: "id",
+        question: { type: "varchar(1000)", notNull: true },
+        answer: { type: "varchar(1000)", notNull: true },
+        created_at: {
+            type: "timestamp",
+            notNull: true,
+            default: pgm.func("current_timestamp"),
+        },
+        language_id: {
+            type: "integer",
+            notNull: true,
+            references: "language",
+            onDelete: "cascade",
+        },
+    });
+};
+
+exports.down = (pgm) => {};
---
+++ migrations/1689789659267_createFlashCardUserMapping.js
@@ -0,0 +1,26 @@
+exports.up = (pgm) => {
+    pgm.createTable("flash_card_user_mapping", {
+        id: "id",
+        flash_card_id: {
+            type: "integer",
+            notNull: true,
+            references: "flash_card",
+            onDelete: "cascade",
+        },
+        user_id: {
+            type: "integer",
+            notNull: true,
+            references: "user",
+            onDelete: "cascade",
+        },
+        created_at: {
+            type: "timestamp",
+            notNull: true,
+            default: pgm.func("current_timestamp"),
+        },
+    });
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable("flash_card_user_mapping");
+};
