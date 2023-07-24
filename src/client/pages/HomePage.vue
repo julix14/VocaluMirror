862c57d031c66565e5c3f5a06f8b0d36e53e2a1b
Juliusbecker <julix.becker@gmail.com>
2023-07-24 13:54:17 +0200

    Make Header on HomePage resonsive to screensize

--- src/client/pages/HomePage.vue
+++ src/client/pages/HomePage.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="bg-orange w-screen h-screen flex flex-col justify-start">
         <div>
-            <p class="text-white text-9xl text-center mt-10 drop-shadow-2xl">Welcome to Vocalu</p>
+            <p class="text-white text-8xl xl:text-9xl text-center mt-10 drop-shadow-2xl">
+                Welcome to Vocalu
+            </p>
             <p class="text-white text-4xl text-center font-bold mt-4">
                 What do you want to train today?
             </p>
@@ -9,12 +11,13 @@
         <div class="flex justify-around m-10">
             <MenuButton title="Battle" img-path="home/battle-img.png" route="/battle" />
             <MenuButton title="Matching" img-path="home/matching-img.png" route="/matching" />
-            <MenuButton title="Flash Cards" img-path="home/flash-card-img.png" route="/flash-cards" />
+            <MenuButton
+                title="Flash Cards"
+                img-path="home/flash-card-img.png"
+                route="/flash-cards" />
         </div>
-        
     </div>
 </template>
-
 
 <script setup>
 import MenuButton from "../components/buttons/MenuButton.vue";
