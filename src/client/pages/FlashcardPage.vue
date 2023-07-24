8fe43913a7e8d77d1e6fc1069be9f5f91631a1e5
Juliusbecker <julix.becker@gmail.com>
2023-07-24 14:30:28 +0200

    Change text

--- src/client/pages/FlashcardPage.vue
+++ src/client/pages/FlashcardPage.vue
@@ -2,8 +2,10 @@
     <div
         class="bg-lightBlue bg-opacity-75 w-screen h-fit min-h-screen flex flex-col justify-start items-center">
         <div>
-            <p class="text-white text-9xl text-center mt-10 drop-shadow-2xl">Flash Cards</p>
-            <p class="text-white text-4xl text-center font-bold mt-4">
+            <p class="text-white text-8xl xl:text-9xl text-center mt-10 drop-shadow-2xl">
+                Flash Cards
+            </p>
+            <p class="text-subHeadingGray text-3xl xl:text-4xl text-center font-bold mt-4">
                 Train you with your flash cards
             </p>
         </div>
