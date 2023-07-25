2decd7b2cb5522041c1a00b46c7c69ecfe8c5526
Juliusbecker <julix.becker@gmail.com>
2023-07-25 17:15:22 +0200

    Renaming

--- src/client/pages/MatchingPage.vue
+++ src/client/pages/MatchingPage.vue
@@ -28,7 +28,7 @@
 
 <script setup>
 import MatchingButton from "../components/buttons/MatchingButton.vue";
-import VocaluImage from "../components/VocaluImage.vue";
+import VocaluImage from "../components/VocaluMatchingImage.vue";
 import { onMounted, ref } from "vue";
 import axios from "axios";
 
