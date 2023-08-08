a1c98f710214f203a49c6bcae81b38218b4fffd0
Juliusbecker <julix.becker@gmail.com>
2023-08-08 11:59:30 +0200

    Add Avatars

---
+++ src/client/components/BattleCharacter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex flex-col justify-center">
+        <progress class="w-36 h-2" :value="health" max="100"></progress>
+        <img :src="getFullUrl(avatar)" alt="characterImage" class="w-36 aspect-square mt-2" />
+        <p class="text-subHeadingGray text-2xl text-center">{{ name }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    name: {
+        type: String,
+        required: true,
+        default: "Player",
+    },
+    avatar: {
+        type: String,
+        required: true,
+        default: "../assets/avatars/avatar1.png",
+    },
+    health: {
+        type: Number,
+        required: true,
+        default: 90,
+    },
+});
+
+function getFullUrl(relativeUrl) {
+    const baseUrl = new URL(import.meta.url);
+    const fullPath = `src/client/assets/${relativeUrl}`;
+    return new URL(fullPath, baseUrl.origin).href;
+}
+</script>
--- src/client/pages/BattlePage.vue
+++ src/client/pages/BattlePage.vue
@@ -1,6 +1,23 @@
 <template>
-    <div
-        class="bg-lightBlue bg-opacity-75 w-screen h-fit min-h-screen flex flex-col justify-start items-center"></div>
+    <div class="bg-yellow bg-opacity-60 w-screen h-fit min-h-screen flex flex-col overflow-hidden">
+        <div>
+            <HomeButton />
+            <p class="text-white text-8xl xl:text-9xl text-center mt-10 drop-shadow-2xl">
+                Word Battle
+            </p>
+            <p class="text-subHeadingGray text-3xl xl:text-4xl text-center font-bold mt-4">
+                Who know more vocabulary words?
+            </p>
+        </div>
+        <div class="grid grid-cols-4 grid-rows-3 h-full w-full ml-10 mr-10 justify-items-center">
+            <BattleCharacter class="row-start-3" avatar="avatars/avatar1.png" name="Player" />
+            <BattleCharacter class="col-start-4" avatar="avatars/avatar2.png" name="Computer" />
+        </div>
+        <div>&nbsp;</div>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import BattleCharacter from "../components/BattleCharacter.vue";
+import HomeButton from "../components/buttons/HomeButton.vue";
+</script>
