7e96e9249768238957894b42e161e8b2a709e098
Juliusbecker <julix.becker@gmail.com>
2023-08-08 16:20:16 +0200

    Add Health Change displaying to Characters

--- src/client/components/BattleCharacter.vue
+++ src/client/components/BattleCharacter.vue
@@ -1,24 +1,33 @@
 <template>
-    <div class="flex flex-col justify-center w-36 h-36 drop-shadow-2xl">
+    <div class="flex flex-col justify-center w-36 h-36 drop-shadow-2xl relative">
         <div class="w-full bg-gray-200 rounded-xl">
             <div
-                class="text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-2xl"
+                class="text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-2xl duration-"
                 :class="{
-                    'bg-red-700': health < 30,
-                    'bg-orange': health >= 30 && health <= 60,
-                    'bg-green-700': health > 60,
+                    'bg-red-700': props.health < 30,
+                    'bg-orange': props.health >= 30 && props.health <= 60,
+                    'bg-green-700': props.health > 60,
                 }"
-                :style="{ width: health + '%' }">
-                {{ health }}
+                :style="{ width: props.health + '%' }">
+                {{ props.health }}
             </div>
         </div>
         <img :src="getFullUrl(avatar)" alt="characterImage" class="aspect-square mt-2" />
+        <div class="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
+            <div
+                v-if="healthDiff"
+                :class="healthDiff > 0 ? 'text-green-500' : 'text-red-500'"
+                class="text-6xl font-bold">
+                {{ healthDiff > 0 ? "+" : "" }}{{ healthDiff }}
+            </div>
+        </div>
         <p class="text-subHeadingGray text-2xl text-center">{{ name }}</p>
     </div>
 </template>
 
 <script setup>
-defineProps({
+import { watch, ref } from "vue";
+const props = defineProps({
     name: {
         type: String,
         required: true,
@@ -36,10 +45,26 @@ defineProps({
     },
 });
 
+const healthDiff = ref(0);
+
+watch(
+    () => props.health,
+    (newVal, oldVal) => {
+        healthDiff.value = newVal - oldVal;
+        setTimeout(() => {
+            resetHealthDiff();
+        }, 1000);
+    }
+);
+
 function getFullUrl(relativeUrl) {
     const baseUrl = new URL(import.meta.url);
     const fullPath = `src/client/assets/${relativeUrl}`;
     return new URL(fullPath, baseUrl.origin).href;
+}
+
+async function resetHealthDiff() {
+    healthDiff.value = 0;
 }
 </script>
 
